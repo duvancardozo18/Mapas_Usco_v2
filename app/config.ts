d6ae3620e16cf1ad82246d44a75e8e3b8ed720ec
Minirick <duvanestivencardozo@gmail.com>
2025-03-03 17:37:26 -0500

    feat: coordenadas usco

--- app/config.ts
+++ app/config.ts
@@ -5,14 +5,10 @@ const config = {
   geoCodingApi: "https://nominatim.openstreetmap.org",
   routingApi: "https://router.project-osrm.org/route/v1",
   mapConfig: {
-    center: [3.110_97, 45.758_887],
+    center: [-75.298_727_787_939_53, 2.941_887_748_252_334],
     zoom: isMobile ? 17 : 18.5,
     bearing: 60,
     pitch: 40,
-    maxBounds: [
-      [3.098_579_765_873_666, 45.753_206_988_746_97],
-      [3.120_672_060_142_396_7, 45.764_883_726_343_584],
-    ],
   } as maplibregl.MapOptions,
   mapStyles: {
     light: "https://tiles.openfreemap.org/styles/bright",
