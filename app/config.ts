550b57ce041f27d405ffc50ad1210c8430bb0059
Minirick <duvanestivencardozo@gmail.com>
2025-03-06 08:00:12 -0500

    refactor: limitacion de mapa usco

--- app/config.ts
+++ app/config.ts
@@ -5,10 +5,14 @@ const config = {
   geoCodingApi: "https://nominatim.openstreetmap.org",
   routingApi: "https://router.project-osrm.org/route/v1",
   mapConfig: {
-    center: [-75.298_727_787_939_53, 2.941_887_748_252_334],
-    zoom: isMobile ? 17 : 18.5,
+    center: [-75.300_137, 2.942_752],
+    zoom: isMobile ? 16.2 : 16.9,
     bearing: 60,
     pitch: 40,
+    maxBounds: [
+      [-75.304, 2.941],
+      [-75.297, 2.9449],
+    ],
   } as maplibregl.MapOptions,
   mapStyles: {
     light: "https://tiles.openfreemap.org/styles/bright",
